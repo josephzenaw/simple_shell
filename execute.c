2d75766dabb695545d2480fd5bde5f722534b6eb
Khalid Mohammed <khalarasta104@gmail.com>
2023-04-27 00:25:27 +0300

    initial commit

---
+++ execute.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+int lsh_exit(char **args);
+
+/*
+ * List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {"exit"};
+
+int (*builtin_func[])(char **) = {&lsh_exit};
+
+/**ls
+ *
+ * lsh_num_builtins - size
+ * Return: size
+ */
+
+int lsh_num_builtins(void)
+{
+	return (sizeof(builtin_str) / sizeof(char *));
+}
+
+/*
+ * Builtin function implementations.
+ */
+
+/**
+ * lsh_exit - builtin to exit the shell
+ * @args: List of args.  Not examined.
+ * Return: Always returns 0, to terminate execution.
+ */
+int lsh_exit(char **args)
+{
+	(void)args;
+	free(args);
+	return (200);
+}
+
+/**
+ *_fork_fun - foo that create a fork.
+ *@arg: Command and values path.
+ *@av: Has the name of our program.
+ *@env: Environment
+ *@lineptr: Command line for the user.
+ *@np: ID of process.
+ *@c: Checker add new test
+ *Return: 0 success
+ */
+
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c)
+{
+
+	pid_t child;
+	int status, i = 0;
+	char *format = "%s: %d: %s: not found\n";
+
+	if (arg[0] == NULL)
+		return (1);
+	for (i = 0; i < lsh_num_builtins(); i++)
+	{
+		if (_strcmp(arg[0], builtin_str[i]) == 0)
+			return (builtin_func[i](arg));
+	}
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(arg[0], arg, env) == -1)
+		{
+			fprintf(stderr, format, av[0], np, arg[0]);
+			if (!c)
+				free(arg[0]);
+			free(arg);
+			free(lineptr);
+			exit(errno);
+		}
+	}
+	else
+	{
+		wait(&status);
+		return (status);
+	}
+	return (0);
+}
